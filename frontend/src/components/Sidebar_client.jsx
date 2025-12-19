a2db6a781896b4b8b73aaa8408fe3d677d720c92
Mrudhula-DS <227003091@sastra.ac.in>
2025-12-19 07:32:16 +0530

    Added features

---
+++ frontend/src/components/Sidebar_client.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+import "./Sidebar_client.css";
+
+export default function SidebarClient() {
+  return (
+    <aside className="sidebar">
+      <nav className="sidebar-nav">
+        <NavLink to="/client-dashboard" className="nav-item">
+          Dashboard
+        </NavLink>
+
+        <NavLink to="/assign-project" className="nav-item">
+          Assign Project
+        </NavLink>
+
+        <NavLink to="/create-project" className="nav-item">
+          Create Project
+        </NavLink>
+
+        <NavLink to="/project-list" className="nav-item">
+          Project List
+        </NavLink>
+
+        <NavLink to="/reports" className="nav-item">
+          Reports
+        </NavLink>
+
+        <NavLink to="/add-employee" className="nav-item">
+          Add Employee
+        </NavLink>
+      </nav>
+    </aside>
+  );
+}
